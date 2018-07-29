cd5f56364c638d39c62982d23a4d9f5799fcf4fd
Diego Siqueira <dsi@hellofresh.com>
2018-07-29 16:35:33 +0200

    QuickUnionWeighted

---
+++ quickunionweighted/quickunionweighted_test.go
@@ -0,0 +1,30 @@
+package quickunionweighted_test
+
+import (
+	"testing"
+
+	"github.com/disiqueira/algorithms/quickunionweighted"
+)
+
+func TestQuickFind(t *testing.T) {
+	q := quickunionweighted.New(10)
+	q.Union(1, 2)
+	if !q.Connected(1, 2) {
+		t.Error("Should be true")
+	}
+	if !q.Connected(2, 1){
+		t.Error("Should be true")
+	}
+	if q.Connected(1, 9){
+		t.Error("Should be false")
+	}
+	q.Union(4, 5)
+	q.Union(5, 6)
+	q.Union(2, 6)
+	if !q.Connected(4, 6){
+		t.Error("Should be true")
+	}
+	if q.Connected(8, 9){
+		t.Error("Should be false")
+	}
+}
